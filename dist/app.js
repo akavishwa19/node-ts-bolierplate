6f191e54b8e22d3463b516451a9c8667309551b1
Akash <akash19102001@gmail.com>
2025-07-14 00:23:09 +0530

    adding packages as dev dependencies

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,8 @@
 import express from "express";
-
-const app=express();
-
-app.get("/",async (req,res)=>{
+const app = express();
+app.get("/", async (req, res) => {
     res.send("lol");
-})
-
-app.listen(3000,()=>{
+});
+app.listen(3000, () => {
     console.log('server listening on http://localhost:3000');
-})
+});
